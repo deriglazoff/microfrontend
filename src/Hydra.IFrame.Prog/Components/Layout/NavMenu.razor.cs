b430b938f04ea2bafcc0c0139d6d5e0ce6cf98c2
SaveliyK <zakalennij@bk.ru>
2024-06-27 18:26:36 +0300

    feat: add frames

---
+++ src/Hydra.IFrame.Prog/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Hydra.IFrame.Prog.Components.Layout
+{
+    public partial class NavMenu
+    {
+        public string OnClickCounter()
+        {
+            return @"<iframe width=""480"" height=""270"" src=""https://www.youtube.com/embed/@video"" frameborder=""0"" allow=""accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"" allowfullscreen></iframe>";
+        }
+    }
+}
