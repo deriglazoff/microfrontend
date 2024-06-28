d1cc318bf2c21ce8d0d0f243c33074b52f9e2a36
SaveliyK <zakalennij@bk.ru>
2024-06-28 16:11:11 +0300

    feat: add service3

---
+++ src/Service2/Data/SportStatistic.cs
@@ -0,0 +1,11 @@
+namespace Service2.Data
+{
+    public class SportStatistic
+    {
+        public DateOnly Date { get; set; }
+
+        public DateTime TimeSpending { get; set; }
+
+        public string? Summary { get; set; }
+    }
+}
--- src/Service2/Program.cs
+++ src/Service2/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<SportStatisticService>();
 
 var app = builder.Build();
 
