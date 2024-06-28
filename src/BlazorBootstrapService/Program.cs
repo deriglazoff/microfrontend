e52aba2f16444e62874c3e9bab8b7189ce2318e8
HellRay <hellraygh@gmail.com>
2024-06-28 16:11:37 +0300

    MatBlazor work, but not full, but how

--- src/BlazorBootstrapService/Program.cs
+++ src/BlazorBootstrapService/Program.cs
@@ -2,19 +2,16 @@ using BlazorBootstrapService.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddBlazorBootstrap(); // Add this line
+builder.Services.AddBlazorBootstrap();
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
--- src/MatBlazorService/Program.cs
+++ src/MatBlazorService/Program.cs
@@ -1,12 +1,27 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MatBlazorService;
+using Microsoft.AspNetCore.Components.Web;
+using System.Net.Http;
+
 using MatBlazor;
+using MatBlazorService;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMatBlazor(); // Добавлено
+var builder = WebApplication.CreateBuilder(args);
 
-await builder.Build().RunAsync();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddMatBlazor();
 
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
